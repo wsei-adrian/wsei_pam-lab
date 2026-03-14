d6b541e06b3fedfc75f7fe7cad214353a7a5656b
Adrian <adrianmazur-dev@proton.me>
2026-03-14 13:53:26 +0100

    lab-03(MainActivity): handle Lab01 button

--- app/src/main/java/pl/wsei/pam/MainActivity.kt
+++ app/src/main/java/pl/wsei/pam/MainActivity.kt
@@ -1,5 +1,6 @@
 package pl.wsei.pam
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -7,7 +8,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pl.wsei.pam.lab01.Lab01Activity
 import pl.wsei.pam.lab01.R
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +25,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickMainBtnRunLab01(v: View){
-        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, Lab01Activity::class.java)
+        startActivity(intent)
     }
 
     fun onClickMainBtnRunLab02(v: View){
