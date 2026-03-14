5871abe1c9a48273ab23ec5c8e8ef7c2bc87e03b
Adrian <adrianmazur-dev@proton.me>
2026-03-14 12:54:57 +0100

    lab-02: implement kotlin tasks (14-16), add testing buttons

--- app/src/main/java/pl/wsei/pam/lab01/MainActivity.kt
+++ app/src/main/java/pl/wsei/pam/lab01/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.Toolbar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     lateinit var mLayout: LinearLayout
     lateinit var mTitle: TextView
+    lateinit var mProgress: ProgressBar
     var mBoxes: MutableList<CheckBox> = mutableListOf()
     var mButtons: MutableList<Button> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +35,59 @@ class MainActivity : AppCompatActivity() {
             val checkBox = CheckBox(this)
             checkBox.text = "Zadanie ${i}"
             checkBox.isEnabled = false
-            mLayout.addView(checkBox)
+
+            val row = LinearLayout(this)
+            row.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            row.orientation = LinearLayout.HORIZONTAL
+
+            val mButton = Button(this).also {
+                it.text = "Testuj"
+                it.textSize = 24f
+                it.setOnClickListener {
+                    var testPassed = false
+
+                    when (i) {
+                        1 -> if (task11(4, 6) in 0.666665..0.666667 && task11(7, -6) in -1.1666667..-1.1666665) testPassed = true
+                        2 -> if (task12(7U, 6U) == "7 + 6 = 13" && task12(12U, 15U) == "12 + 15 = 27") testPassed = true
+                        3 -> if (task13(0.0, 5.4f) && !task13(7.0, 5.4f) && !task13(-6.0, -1.0f) && task13(6.0, 9.1f) && !task13(6.0, -1.0f) && task13(1.0, 1.1f)) testPassed = true
+                        4 -> if (task14(-2, 5) == "-2 + 5 = 3" && task14(-2, -5) == "-2 - 5 = -7") testPassed = true
+                        5 -> if (task15("DOBRY") == 4 && task15("barDzo dobry") == 5 && task15("doStateczny") == 3 && task15("Dopuszczający") == 2 && task15("NIEDOSTATECZNY") == 1 && task15("XYZ") == -1) testPassed = true
+                        6 -> if (
+                            task16(mapOf("A" to 2U, "B" to 4U, "C" to 3U), mapOf("A" to 1U, "B" to 2U)) == 2U &&
+                            task16(mapOf("A" to 2U, "B" to 4U, "C" to 3U), mapOf("F" to 1U, "G" to 2U)) == 0U &&
+                            task16(mapOf("A" to 23U, "B" to 47U, "C" to 30U), mapOf("A" to 1U, "B" to 2U, "C" to 4U)) == 7U
+                        ) testPassed = true
+                    }
+
+                    if (testPassed) {
+                        if (!checkBox.isChecked) {
+                            checkBox.isChecked = true
+                            mProgress.progress += 100 / 6
+                        }
+                        Toast.makeText(this, "Zadanie $i zaliczone!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Zadanie $i oblało test.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            row.addView(checkBox)
+            row.addView(mButton)
+            mLayout.addView(row)
             mBoxes.add(checkBox)
+            mButtons.add(mButton)
         }
+
+        mProgress = ProgressBar(
+            this,
+            null,
+            androidx.appcompat.R.attr.progressBarStyle,
+            androidx.appcompat.R.style.Widget_AppCompat_ProgressBar_Horizontal
+        )
+        mLayout.addView(mProgress)
 
         if (
             task11(4, 6) in 0.666665..0.666667 &&
@@ -121,7 +175,11 @@ class MainActivity : AppCompatActivity() {
     // Wskazówki:
     // Math.abs(a) - zwraca wartość bezwględną
     fun task14(a: Int, b: Int): String {
-       return ""
+        return if (b < 0) {
+            "$a - ${abs(b)} = ${a + b}"
+        } else {
+            "$a + $b = ${a + b}"
+        }
     }
 
     // Zdefiniuj funkcję zwracającą ocenę jako liczbę całkowitą na podstawie łańcucha z opisem słownym oceny.
@@ -134,7 +192,14 @@ class MainActivity : AppCompatActivity() {
     // Funkcja nie powinna być wrażliwa na wielkość znaków np. Dobry, DORBRY czy DoBrY to ta sama ocena
     // Wystąpienie innego łańcucha w degree funkcja zwraca wartość -1
     fun task15(degree: String): Int {
-        return 0
+        return when (degree.lowercase()) {
+            "bardzo dobry" -> 5
+            "dobry" -> 4
+            "dostateczny" -> 3
+            "dopuszczający" -> 2
+            "niedostateczny" -> 1
+            else -> -1
+        }
     }
 
     // Zdefiniuj funkcję zwracającą liczbę możliwych do zbudowania egzemplarzy, które składają się z elementów umieszczonych w asset
@@ -146,6 +211,16 @@ class MainActivity : AppCompatActivity() {
     // println(items)	=> 2 ponieważ do zbudowania jednego egzemplarza potrzebne są 2 elementy "B" i jeden "A", a w magazynie mamy 2 "A" i 4 "B",
     // czyli do zbudowania trzeciego egzemplarza zabraknie elementów typu "B"
     fun task16(store: Map<String, UInt>, asset: Map<String, UInt>): UInt {
-        return UInt.MAX_VALUE
+        var minItems = UInt.MAX_VALUE
+        for ((key, requiredQty) in asset) {
+            val availableQty = store[key] ?: 0U
+            if (requiredQty > 0U) {
+                val possibleItems = availableQty / requiredQty
+                if (possibleItems < minItems) {
+                    minItems = possibleItems
+                }
+            }
+        }
+        return if (minItems == UInt.MAX_VALUE) 0U else minItems
     }
 }
